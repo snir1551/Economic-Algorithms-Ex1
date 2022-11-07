79016da1b99bfd247cc1f7178172beda6bdc371e
snir1551 <snir1552@gmail.com>
2022-11-07 12:57:26 +0200

    update

--- agent.py
+++ agent.py
@@ -5,19 +5,9 @@ from typing import List
 class Agent:
     def __init__(self):
         self.options: Dict[int, float] = dict()
-        self.num: float = -1
-        self.replay_num = 0
 
     def set_option(self, option: int, value: float):
         self.options.update({option: value})
-        if self.num < value:
-            self.num = value
-            self.replay_num = 1
-        elif self.num == value:
-            self.replay_num += 1
-
-    def get_num(self):
-        return self.num
 
     def value(self, option: int) -> float:
         if self.options.get(option) is None:
@@ -29,7 +19,6 @@ def isParetoImprovement(agents: List[Agent], option1: int, option2: int) -> bool
     numAgents = 0
     for x in agents:
         if x.value(option1) < x.value(option2):
-            print(x.value(option1))
             return False
         elif x.value(option1) == x.value(option2):
             numAgents += 1
@@ -41,14 +30,9 @@ def isParetoImprovement(agents: List[Agent], option1: int, option2: int) -> bool
 
 
 def isParetoOptimal(agents: List[Agent], option: int, allOptions: List[int]) -> bool:
-    equal_in_another_option = 0
-    for x in agents:
-        if x.value(option) < x.get_num():
-            return True
-        if x.replay_num > 1:
-            equal_in_another_option += 1
+    for option_another_agent in allOptions:
+        if option != option_another_agent and isParetoImprovement(agents, option_another_agent, option):
+            return False
 
-    if equal_in_another_option == len(agents):
-        return True
+    return True
 
-    return False
